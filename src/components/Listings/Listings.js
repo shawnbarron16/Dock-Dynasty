b023c39b8de99b644db94f143ebff40c7f41d005
shawnbarron16 <shawnbarron16@gmail.com>
2021-06-18 10:11:08 -0500

    Shows all listings

--- src/components/Listings/Listings.js
+++ src/components/Listings/Listings.js
@@ -1,30 +1,33 @@
-import React, {useContext, useEffect} from "react";
-import { ListingContext } from "./ListingsProvider"
+import React, { useContext, useEffect } from "react";
+import { ListingContext } from "./ListingsProvider";
 import { BoatContext } from "../Boats/BoatsProvider";
+import "./ListingsStyle.css"
 
 //A singular component that creates the JSX for all Listings
 export const Listings = () => {
-    const { listings, getListings } = useContext(ListingContext)
-    const { boats, getBoats } = useContext(BoatContext)
+  const { listings, getListings } = useContext(ListingContext);
+  const { boats, getBoats } = useContext(BoatContext);
 
+  useEffect(() => {
+    console.log("Retrieving Listings");
+    getBoats();
+    getListings();
+  }, []);
 
-    useEffect(() => {
-        console.log("Retrieving Listings")
-        getBoats()
-        getListings()
-    }, [])
-
-    return (
-        <section className="listings">
-            {listings.map(listing => {
-                let thisBoatId = parseInt(listing.boatId)
-                let thisBoat = boats.find(boat => boat.id === thisBoatId)
-                return (
-                <div className="listings__listing" key={listing.id} id={`listing--${listing.id}`}>
-                    <img src={thisBoat.pictureURL} alt=""></img>
-                </div>
-                )
-            })}
-        </section>
-    )
-}
+  return (
+    <section className="listings">
+      {listings.map((listing) => {
+        let thisBoatId = parseInt(listing.boatId);
+        let thisBoat = boats.find((boat) => boat.id === thisBoatId);
+        return (
+          <div className="listings__listing" key={listing.id} id={`listing--${listing.id}`}
+          style={{display: "flex", flexFlow: "column wrap", borderColor: "black", borderStyle: "solid", borderWidth: 2}}>
+            <img className="listing__image" src={thisBoat.pictureURL} alt="" style={{height: 250, width: 300 }}></img>
+            {thisBoat.description}
+            <button>See More</button>
+          </div>
+        );
+      })}
+    </section>
+  );
+};
