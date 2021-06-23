62ef036dc373faaafc553f5f914808550ba6a0c3
shawnbarron16 <shawnbarron16@gmail.com>
2021-06-23 13:18:50 -0500

    View Individual Listings

--- src/components/Listings/IndividualListing.js
+++ src/components/Listings/IndividualListing.js
@@ -1,13 +1,53 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { ListingContext } from "./ListingsProvider";
 import { BoatContext } from "../Boats/BoatsProvider";
+import { useParams } from "react-router-dom";
 
+//A component that handles the viewing of a singular listing with expanded information
 export const IndividualListing = () => {
-    const { listings, getListings } = useContext(ListingContext);
-    const { boats, getBoats } = useContext(BoatContext)
+  const { getListingById } = useContext(ListingContext);
+  const { getBoatById } = useContext(BoatContext);
+  const [listing, setListing] = useState({});
+  const [boat, setBoat] = useState([]);
+  const { listingId } = useParams();
 
-    useEffect(() => {
-        getListings()
-        getBoats()
-    },[])
-}
+  useEffect(() => {
+    console.log("Getting Individual Listing");
+    getListingById(listingId).then((data) => setListing(data));
+    console.log(listing);
+  }, []);
+
+  useEffect(() => {
+    getBoatById(listing.boatId).then((data) => setBoat(data));
+  }, [listing]);
+
+  return (
+    <>
+      <section className="singleListing__boat">
+        <article className="singleListing__boat__picture">
+          <div>
+            <img
+              src={boat.pictureURL}
+              alt=""
+              style={{ height: 250, width: 300 }}
+            ></img>
+          </div>
+        </article>
+        <article className="singleListing__boat__description">
+          {boat.description}
+        </article>
+      </section>
+      <section className="singleListing__info">
+        <article className="singleListing__info__date">
+          {listing.dateListed}
+        </article>
+        <article className="singleListing__info__price">
+          {listing.price}
+        </article>
+        <article className="singleListing__info__rent">
+          <button>RENT</button>
+        </article>
+      </section>
+    </>
+  );
+};
--- src/components/Listings/Listings.js
+++ src/components/Listings/Listings.js
@@ -2,14 +2,16 @@ import React, { useContext, useEffect } from "react";
 import { ListingContext } from "./ListingsProvider";
 import { BoatContext } from "../Boats/BoatsProvider";
 import "./ListingsStyle.css"
+import { useHistory } from "react-router-dom";
 
-//A singular component that creates the JSX for all Listings
+//A singular component that creates the JSX for all Listings of the homepage 
 export const Listings = () => {
   const { listings, getListings } = useContext(ListingContext);
   const { boats, getBoats } = useContext(BoatContext);
+  const history = useHistory()
 
   useEffect(() => {
-    console.log("Retrieving Listings");
+    console.log("Retrieving Listings...Retrieving Boats...");
     getBoats();
     getListings();
   }, []);
@@ -21,10 +23,11 @@ export const Listings = () => {
         let thisBoat = boats.find((boat) => boat.id === thisBoatId);
         return (
           <div className="listings__listing" key={listing.id} id={`listing--${listing.id}`}
-          style={{display: "flex", flexFlow: "column wrap", borderColor: "black", borderStyle: "solid", borderWidth: 2}}>
+          style={{display: "flex", flexFlow: "column wrap", borderColor: "black", borderStyle: "solid", 
+          borderWidth: 2, marginLeft: 10, marginRight: 10, maxWidth:300, maxHeight:400, justifyContent: "space-around"}}>
             <img className="listing__image" src={thisBoat.pictureURL} alt="" style={{height: 250, width: 300 }}></img>
             {thisBoat.description}
-            <button>See More</button>
+            <button onClick={ () => {history.push(`/listings/${listing.id}`)}}>See More</button>
           </div>
         );
       })}
